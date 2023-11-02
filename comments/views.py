2b4d0f455fc7e70a289bf70f455c7c6c328aef82
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-11-02 00:57:38 +0000

    Added filter search option to comments.views

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions, generics
+from django_filters.rest_framework import DjangoFilterBackend
 from snapdish.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -8,6 +9,8 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
