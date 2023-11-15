a60e12fa9bdc8d823d5de5384ae600a53202c81c
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-11-15 17:34:20 +0000

    Added env.py for testing

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ['CLOUDINARY_URL'] = 'cloudinary://137853885331339:A1MeZ1tuYMumw9sb_8poDasvoRM@dhfp7asfd'
+os.environ['SECRET_KEY'] = "YunusPumpkin20"
+os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = "postgres://zpublajh:IE-f56jjeQCZ24FRYPbQMs5zQ7tlHN7B@surus.db.elephantsql.com/zpublajh"
+os.environ.setdefault("SECRET_KEY", "YunusPumpkin20")
