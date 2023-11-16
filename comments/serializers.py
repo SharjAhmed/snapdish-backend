08923e794c5882d39974af498c859031ab0cb087
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-11-16 19:22:33 +0000

    added missing parenthesis for created_at & updated_at in comments.serializers

--- comments/serializers.py
+++ comments/serializers.py
@@ -10,8 +10,8 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(
         source='owner.profile.image.url'
     )
-    created_at = serializers.SerializerMethodField
-    updated_at = serializers.SerializerMethodField
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
